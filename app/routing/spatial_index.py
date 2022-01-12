6a2c230af578d80789f48845a1d5508c7948b23e
wolfhard.fehre <wolfhard.fehre@motion-tag.com>
2022-01-12 18:58:23 +0100

    fix oneway streets in routing graph

--- app/routing/spatial_index.py
+++ app/routing/spatial_index.py
@@ -1,6 +1,8 @@
+from typing import Tuple
+
 import pandas as pd
 import numpy as np
-from scipy.spatial import cKDTree
+from scipy.spatial import KDTree
 from app.overpass.location import Location
 from app.routing.feeds.feed import LATITUDE, LONGITUDE
 from app.config import logging, NODES_PATH
@@ -12,17 +14,19 @@ class SpatialIndex:
         logging.debug('building spatial index')
         geometries = nodes[[LONGITUDE, LATITUDE]].to_numpy()
         self._nodes = nodes
-        self._algorithm = cKDTree(geometries)
+        self._algorithm = KDTree(geometries)
         self.bounds = self._bounds()
 
-    def closest_to(self, location: Location):
-        logging.debug(f'fetching closest point to {location}')
+    def closest_to(self, location: Location) -> Tuple[pd.Series, float]:
+        logging.debug('fetching closest point to %s', location)
         distance, index = self._algorithm.query(np.array(location))
         closest = self._nodes.iloc[index]
-        logging.debug(f'closest: {closest.name}, dist: {distance}°, loc: ({closest["lat"]}, {closest["lon"]})')
+        logging.debug(
+            'closest: %s, dist: %.7f°, loc: (%.7f, %.7f)', closest.name, distance, closest['lat'], closest['lon']
+        )
         return closest, distance
 
-    def _bounds(self):
+    def _bounds(self) -> Tuple[float, float, float, float]:
         west = min(self._nodes[LONGITUDE])
         east = max(self._nodes[LONGITUDE])
         south = min(self._nodes[LATITUDE])
@@ -37,4 +41,4 @@ if __name__ == '__main__':
     loc = Location(longitude=13.412003, latitude=52.522625)
     node, degrees = spatial_index.closest_to(loc)
 
-    print(f'node: {node.name}, distance: {degrees}')
+    print(f'node: {node.name}, distance: {degrees:.7f}°')
