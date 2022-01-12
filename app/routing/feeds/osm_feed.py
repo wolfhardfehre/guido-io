c5ab014176bdf008212fd4cf81a9b8b8df99ea87
Nice Fontaine <wolfhard.fehre@motion-tag.com>
2022-01-12 18:58:49 +0100

    fix oneway streets in routing graph (#21)

--- app/routing/feeds/osm_feed.py
+++ app/routing/feeds/osm_feed.py
@@ -1,3 +1,5 @@
+from __future__ import annotations
+
 import logging
 import pandas as pd
 
@@ -6,10 +8,22 @@ from app.osm.pbf_parser import PbfParser
 from app.routing.feeds.feed import Feed, NODE_ID, LATITUDE, LONGITUDE, FROM_NODE, TO_NODE, DISTANCE
 
 GOOD_HIGHWAYS = [
-    'tertiary', 'primary', 'secondary', 'trunk',
-    'residential', 'living_street', 'path',
-    'unclassified', 'track', 'primary_link',
-    'secondary_link', 'cycleway'
+    'motorway',
+    'trunk',
+    'primary',
+    'secondary',
+    'tertiary',
+    'unclassified',
+    'residential',
+    'motorway_link',
+    'trunk_link',
+    'primary_link',
+    'secondary_link',
+    'tertiary_link',
+    'living_street',
+    #'track',
+    #'path',
+    #'cycleway'
 ]
 
 
@@ -17,7 +31,7 @@ class OsmFeed(Feed):
     __TYPE__ = 'osm'
 
     @classmethod
-    def area(cls, **kwargs):
+    def area(cls, **kwargs) -> OsmFeed:
         use_cache = kwargs.get('use_cache', True)
         file_name = Geofabrik.fetch(**kwargs)
         parser = PbfParser(file_name, use_cache=use_cache)
@@ -36,43 +50,38 @@ class OsmFeed(Feed):
         self._clean_nodes()
 
     @property
-    def name(self):
+    def name(self) -> str:
         return self._name
 
-    def _check_columns(self):
+    def _check_columns(self) -> None:
         if not all([col in self._osm_ways.columns for col in (FROM_NODE, TO_NODE)]):
             raise ValueError(f'Columns of ways must contain {FROM_NODE} and {TO_NODE}!')
         if not all([col in self._osm_nodes.columns for col in (NODE_ID, LATITUDE, LONGITUDE)]):
             raise ValueError(f'Columns of nodes must contain {NODE_ID}, {LATITUDE} and {LONGITUDE}!')
 
-    def _nodes(self):
+    def _nodes(self) -> pd.DataFrame:
         logging.debug('building nodes')
         logging.debug(f'dropping node columns, {self._osm_nodes.shape[0]} rows')
         nodes = self._osm_nodes
         nodes.set_index(NODE_ID, inplace=True)
         return nodes
 
-    def _edges(self):
+    def _edges(self) -> pd.DataFrame:
         logging.debug(f'dropping edge columns, {self._osm_ways.shape[0]} rows')
         edges = self._osm_ways
         edges = self._join(edges, self.nodes)
         edges = self._compute_distances(edges)
-        reversible_edges = edges[edges['oneway']].copy(deep=True)
+        reversible_edges = edges[~edges['oneway']].copy(deep=True)
         edges = self._build_other_direction(edges, reversible_edges)
         return edges[[FROM_NODE, TO_NODE, DISTANCE]]
 
-    def _clean_nodes(self):
+    def _clean_nodes(self) -> None:
         edge_nodes_ids = self.edges[FROM_NODE].unique()
         self.nodes = self.nodes.loc[edge_nodes_ids, :]
 
 
 if __name__ == '__main__':
-    pd.options.display.max_rows = None
-    pd.options.display.max_columns = None
-    pd.options.display.width = 800
-
-    feed = OsmFeed.area(state='berlin')
+    feed = OsmFeed.area(continent='europe', country='germany', state='berlin')
     logging.debug('finished')
-
     print(feed.nodes.head())
     print(feed.edges.head())
